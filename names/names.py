971e7da3e3306b640588e0827ae932b6c904be42
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-15 19:26:40 -0300

    tests passing for all files except ring_buffer.py

--- names/names.py
+++ names/names.py
@@ -2,6 +2,40 @@ import time
 
 start_time = time.time()
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value <= self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else: 
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+            
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            return self.right.contains(target)
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -10,12 +44,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names2set = set(names_2)
+names2tree = BinarySearchTree('jevon')
+for name in names2set:
+    names2tree.insert(name)
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+        if names2tree.contains(name_1):
             duplicates.append(name_1)
 
 end_time = time.time()
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.max = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        self.data.append(item)
+        if len(self.data) == self.max:
+            self.cur = 0
+            # Permanently change self's class from non-full to full
+            self.__class__ = self.__Full
 
-    def get(self):
-        pass
+    class __Full:
+        def append(self, item):
+            self.data[self.cur] = item
+            self.cur = (self.cur+1) % self.max
+
+        def get(self):
+            return self.data[self.cur:]+self.data[:self.cur]
+
